22ceb8cc690c96d0b9acaf4a48acc27307e7d083
casanex <lonelyishtar@gmail.com>
2023-05-08 21:48:26 +0300

    add picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,31 @@
 package hexlet.code;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+    @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff ver 1.0",
+        description = "Compares two configuration files and shows a difference.")
+
+public class App implements Callable<Integer> {
+        @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+        boolean versionInfoRequested;
+        @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+        boolean usageHelpRequested;
+
+        @Override
+        public Integer call() throws Exception {
+            return null;
+        }
+
+        public static void main(String... args) {
+            int exitCode = new CommandLine(new App()).execute(args);
+            System.exit(exitCode);
+        }
     }
-}
---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,7 @@
+package hexlet.code;
+
+public class Differ {
+    public static void generate(String filePath1, String filePath2) {
+
+    }
+}
