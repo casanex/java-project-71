9b8bee288384685c394174d1f4b5af90ccb6b581
casanex <lonelyishtar@gmail.com>
2023-05-11 22:26:22 +0300

    fucking project

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -13,19 +13,28 @@ import java.util.concurrent.Callable;
     @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff ver 1.0",
         description = "Compares two configuration files and shows a difference.")
 
-public class App implements Callable<Integer> {
+        public class App implements Callable<Integer> {
+        @Option(names = {"-f", "--format"}, paramLabel = "format",
+                defaultValue = "stylish", description = "output format [default: stylish]")
+        private String format;
+
+        @Parameters(paramLabel= "filepath1", description = "path to first file")
+        private String filepath1;
+        @Parameters(paramLabel= "filepath2", description = "path to second file")
+        private String filepath2;
+
         @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
         boolean versionInfoRequested;
         @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
         boolean usageHelpRequested;
 
-        @Override
-        public Integer call() throws Exception {
-            return null;
-        }
 
         public static void main(String... args) {
             int exitCode = new CommandLine(new App()).execute(args);
             System.exit(exitCode);
+        }
+        @Override
+        public Integer call() throws Exception {
+                        return 0;
         }
     }
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,7 +1,16 @@
 package hexlet.code;
 
-public class Differ {
-    public static void generate(String filePath1, String filePath2) {
+import java.util.List;
+import java.util.Map;
 
+public class Differ {
+    public static String generate(String filePath1, String filePath2, String Format) {
+        Map<String, Object> map1 = getFileData(filePath1);
+        Map<String, Object> map2 = getFileData(filePath2);
+        List<Map<String, Object>> result = FinderDifference.differ(map1, map2);
+        return
+    }
+
+    private static Map<String, Object> getFileData(String filePath1) {
     }
 }
