66fb91958e5a7219b83aaec8d7f5e0be49521709
casanex <lonelyishtar@gmail.com>
2023-05-13 23:46:14 +0300

    add implements

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 public class Differ {
-    public static String generate(String filePath1, String filePath2, String Format) {
+    public static String generate(String filePath1, String filePath2, String Format) throws Exception {
         Map<String, Object> map1 = getUrlData(filePath1);
         Map<String, Object> map2 = getUrlData(filePath2);
         List<Map<String, Object>> result = FinderDifference.differ(map1, map2);
