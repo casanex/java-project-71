50e522fa942ccfabb43307b730a52e16bf408cb0
casanex <lonelyishtar@gmail.com>
2023-05-13 21:31:37 +0300

    renew code

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,16 +1,18 @@
 package hexlet.code;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 
 public class Differ {
     public static String generate(String filePath1, String filePath2, String Format) {
-        Map<String, Object> map1 = getFileData(filePath1);
-        Map<String, Object> map2 = getFileData(filePath2);
+        Map<String, Object> map1 = getUrlData(filePath1);
+        Map<String, Object> map2 = getUrlData(filePath2);
         List<Map<String, Object>> result = FinderDifference.differ(map1, map2);
         return
     }
 
-    private static Map<String, Object> getFileData(String filePath1) {
-    }
+
 }
