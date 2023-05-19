441b674622b9e848583cfb78e9321dcdf59fcc32
casanex <lonelyishtar@gmail.com>
2023-05-19 23:02:36 +0300

    add readme

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -24,13 +24,15 @@ public class App implements Callable<Integer> {
     @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
         boolean usageHelpRequested;
 
+    @Override
+    public Integer call() throws Exception {
+        System.out.println(Differ.generate(filepath1, filepath2, format));
+        return 0;
+    }
 
     public static void main(String... args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
-    @Override
-        public Integer call() throws Exception {
-        return 0;
-    }
+
 }
