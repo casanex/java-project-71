6d8fefffaabce62ff6d9a846a4ac3eeba6f96b75
casanex <lonelyishtar@gmail.com>
2023-05-19 23:10:23 +0300

    add finall call

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -25,7 +25,7 @@ public class App implements Callable<Integer> {
         private boolean usageHelpRequested;
 
     @Override
-    public Integer call() throws Exception {
+    public final Integer call() throws Exception {
         System.out.println(Differ.generate(filepath1, filepath2, format));
         return 0;
     }
